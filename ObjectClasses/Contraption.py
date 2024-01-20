5b4cf80a2916369b85d1f667832856793249a94b
FrancoDB <francodbalso@gmail.com>
2024-01-20 13:54:57 -0500

    make contraption move sinusoidally

--- ObjectClasses/Contraption.py
+++ ObjectClasses/Contraption.py
@@ -15,8 +15,12 @@ class Contraption(pygame.sprite.Sprite):
     def __init__(self, image):
         super().__init__()
 
-        self.pos = pygame.Vector2(17/20*WIDTH, 3/10*HEIGHT)
-        self.vel = pygame.Vector2(30, 40)
+        self.xtimeParam = 0
+        self.ytimeParam = 0
+        self.anchorPoint = pygame.Vector2(19/20*WIDTH - 8, 4/10*HEIGHT)
+        self.amplitude = 25
+        self.pos = pygame.Vector2(19/20*WIDTH, 4/10*HEIGHT)
+        self.vel = pygame.Vector2(0, 0)
         
         
         self.moveType = "hovering"
@@ -30,10 +34,27 @@ class Contraption(pygame.sprite.Sprite):
         
         if self.moveType == "hovering":
             
-            self.pos += self.vel*dt
+            self.xtimeParam += dt
+            self.ytimeParam += dt
+
+            if self.xtimeParam > 2*np.pi:
+                self.xtimeParam = 0
+            if self.ytimeParam > np.pi:
+                self.ytimeParam = 0
+
+            self.pos.x = self.anchorPoint.x + (self.amplitude/2)*np.sin(self.xtimeParam)
+            self.pos.y = self.anchorPoint.y + (self.amplitude)*np.sin(2*self.ytimeParam)
+
             self.rect.center = (self.pos.x, self.pos.y)
             
-            if self.pos.x > 16/20*WIDTH or self.pos.x < 19/20*WIDTH:
-                self.vel.x *= -1
-            if self.pos.y > 4/10*HEIGHT or self.pos.y < 3/10*HEIGHT:
-                self.vel.y *= -1
+            # if self.pos.x > 19/20*WIDTH and self.vel.x > 0:
+            #     self.vel.x *= -1
+            
+            # if self.pos.x < 17/20*WIDTH and self.vel.x < 0:
+            #     self.vel.x *= -1
+
+            # if self.pos.y > 5/10*HEIGHT and self.vel.y > 0:
+            #     self.vel.y *= -1
+            
+            # if self.pos.y < 4/10*HEIGHT and self.vel.y < 0:
+            #     self.vel.y *= -1
