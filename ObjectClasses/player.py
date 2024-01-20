4ff84377ecca12966e9aeabeb9428ce19bfffe81
sandhyarottoo <sandhya.rottoo@gmail.com>
2024-01-20 11:42:52 -0500

    gravity and jumping works

--- ObjectClasses/player.py
+++ ObjectClasses/player.py
@@ -1,7 +1,6 @@
 import pygame
 import numpy as np
 import os
-import Obstacles
 from globalVars import getVars
 
 WIDTH, HEIGHT, dt, PLAYERXVEL,PLAYERYVEL,playerheight,playerwidth = getVars(['width', 'height', 'dt','playervelocity','jumpvel','playerheight','playerwidth'])
@@ -18,40 +17,53 @@ class Player(pygame.sprite.Sprite):
         self.charge = 1
 
 
-    def update(self,keys,objects):
+    def update(self,events,objects):
         #applyForces(self,'gravity')
         for object in objects:
             self.collision(object)
+
+        
+        ground = pygame.Rect(0, HEIGHT - 10, WIDTH, 10)
+        if ground.colliderect(self.rect):
+            self.rect.y = ground.top-playerheight/2
+            self.vel.y = 0
 
         self.acc = self.applyForces(['gravity'])
         self.vel += self.acc
         
         self.rect.x += self.vel.x*dt
         self.rect.y += self.vel.y*dt
-        
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += self.vel.x*dt
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= self.vel.x*dt
-        if keys[pygame.K_UP]:
-            self.rect.y -= self.vel.y*dt
+
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.rect.x += PLAYERXVEL
+                if event.key == pygame.K_LEFT:
+                    self.rect.x -= PLAYERXVEL
+                if event.key == pygame.K_SPACE:
+                    self.rect.y -= PLAYERYVEL
 
 
     def collision(self,object):
-        if pygame.sprite.collide_mask(self,object) and self.vel.y <0:
+        if pygame.sprite.collide_mask(self,object) and self.vel.y > 0:
             newpos = object.rect.top - playerheight/2
-            self.rect.x = newpos
+            self.rect.y = newpos
             self.vel.y = 0
 
 
     def applyForces(self,listofforces):
         mass = self.mass
         charge = self.charge
-        xacc,yacc = 0
+        xacc,yacc = 0,0
         if 'gravity' in listofforces:
             xacc += 0
             yacc += 10
+    
+        #if 'electricity' in listofforces:
+
+
         return pygame.Vector2(xacc,yacc)
+
 
     #if force == 'electric':
 
