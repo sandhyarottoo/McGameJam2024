a2382d13096040daf33be005bd754c57ba8fa385
CoullObservatory <114324520+CoullObservatory@users.noreply.github.com>
2024-01-19 23:12:14 -0500

    added folder for all object classes

--- globalVars.py
+++ globalVars.py
@@ -6,10 +6,15 @@ def getVars(listOfNames):
     match the vars you want from the below dictionary
     '''
 
-    vars = {'width': 900, 'height': 700, 'dt': 0.01}
+    # variable dictionary
+    vars = {'width': 900, 'height': 700, 
+            'dt': 0.01, 
+            'game_name': "Chasing the Contraption"}
+    
+    # list of variables to return
     returned = []
 
     for name in listOfNames:
-        returned.append(vars[name])
+        returned.append(vars[name.lower()]) # .lower() ensures that the variables are not case sensitive
 
     return returned
