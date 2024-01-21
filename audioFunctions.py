3f77d062413f83a0aaacba88e46f8d4ab9be6840
Piotr-Jakuc <piotr.jakuc@mail.mcgill.ca>
2024-01-21 13:07:59 -0500

    added music

--- audioFunctions.py
+++ audioFunctions.py
@@ -7,8 +7,15 @@ from globalVars import getVars
 
 WIDTH, HEIGHT, g_obs_dims = getVars(['width', 'height', 'g_obs_dims'])
 
+pygame.init()
 pygame.mixer.init()
-jump_grunt = pygame.mixer.Sound(os.path.realpath(__file__) + "media/Audio/Grunt 1.wav")
 
-def jumpSound():
-    pygame.mixer.Sound.play(jump_grunt)
+def playJump():
+    sound_file_path = os.path.dirname(os.path.realpath(__file__)) + "/media/Audio/Grunt 1.wav"
+    pygame.mixer.music.load(sound_file_path)
+    pygame.mixer.music.play()
+
+def playMusic():
+    sound_file_path = os.path.dirname(os.path.realpath(__file__)) + "/media/Audio/Music.wav"
+    pygame.mixer.music.load(sound_file_path)
+    pygame.mixer.music.play(loops=-1)
